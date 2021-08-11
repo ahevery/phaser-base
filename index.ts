1faadb3436e04afaa1f46e9f41cf2c9d9f188aa8
ahevery <adam@hevery.com>
2021-08-11 09:06:00 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as Phaser from 'phaser';
-import {Types} from 'phaser';
+import { Types } from 'phaser';
 
-var config: Types.Core.GameConfig  = {
+var config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -15,9 +15,13 @@ var config: Types.Core.GameConfig  = {
 var game = new Phaser.Game(config);
 
 function preload(this: Phaser.Scene) {
-  this.load.setBaseURL('https://labs.phaser.io');
+  this.load.setBaseURL(
+    'https://raw.githubusercontent.com/ahevery/phaser-base/master'
+  );
 
   this.load.image('sky', 'assets/sky.png');
+  //this.load.image('sky', 'assets/skies/space3.png');
+
   this.load.image('ground', 'assets/platform.png');
   this.load.image('star', 'assets/star.png');
   this.load.image('bomb', 'assets/bomb.png');
@@ -29,7 +33,6 @@ function preload(this: Phaser.Scene) {
 
 function create() {
   this.add.image(400, 300, 'sky');
-
 }
 
 function update() {}
